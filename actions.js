10e4526a733f2298f1689ac2638527bbcb7dfdc2
IkoBenny <benny.iko.it@gmail.com>
2026-04-27 17:24:53 -0400

    Adds external JavaScript to control event-driven button behavior.
    On branch initial-layout
    Changes to be committed:
    	new file:   actions.js
    	modified:   index.html
    
    Inside a <form>, a plain <button> is treated as:
    
    <button type="submit">
    
    So when you click it, the browser submits the form → which often reloads the page or jumps to the top.
    
    ✅ Option 1: Make it not a submit button
    <button type="button" id="myBtn">Click</button>
    
    To change default behavior, this is the simplest fix if you're just using JS.
    
    Useful Links:
    https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Events
    https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
    https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/button

---
+++ actions.js
@@ -0,0 +1,41 @@
+const findPlayersButton = document.getElementById("find-players-button");
+const submitPlayersButton = document.getElementById("submit-players-button");
+const findTeamsButton = document.getElementById("find-teams-button");
+const submitTeamsButton = document.getElementById("submit-teams-button");
+const findGamesButton = document.getElementById("find-games-button");
+const submitGamesButton = document.getElementById("submit-games-button");
+const findCoachesButton = document.getElementById("find-coaches-button");
+const submitCoachesButton = document.getElementById("submit-coaches-button");
+
+findPlayersButton.addEventListener("click", (event) => {
+	console.log("findPlayersButton");
+});
+
+submitPlayersButton.addEventListener("click", (event) => {
+	console.log("submitPlayersButton");
+});
+
+findTeamsButton.addEventListener("click", (event) => {
+	console.log("findTeamsButton");
+});
+
+submitTeamsButton.addEventListener("click", (event) => {
+	console.log("submitTeamsButton");
+});
+
+findGamesButton.addEventListener("click", (event) => {
+	console.log("findGamesButton");
+});
+
+submitGamesButton.addEventListener("click", (event) => {
+	console.log("submitGamesButton");
+});
+
+findCoachesButton.addEventListener("click", (event) => {
+	console.log("findCoachesButton");
+});
+
+submitCoachesButton.addEventListener("click", (event) => {
+	console.log("submitCoachesButton");
+});
+
