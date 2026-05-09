0b2d079f03f8307364b1c01a327828e9dbe51db0
IkoBenny <benny.iko.it@gmail.com>
2026-05-08 20:46:14 -0400

    implements data population for players find On branch implement-button-behavior  Changes to be committed: 	modified:   actions.js

--- actions.js
+++ actions.js
@@ -1,5 +1,5 @@
 const findPlayersButton = document.getElementById("find-players-button");
-const playerName = document.getElementById("player-name-find");
+const playerNameFind = document.getElementById("player-name-find");
 const submitPlayersButton = document.getElementById("submit-players-button");
 const findTeamsButton = document.getElementById("find-teams-button");
 const submitTeamsButton = document.getElementById("submit-teams-button");
@@ -9,16 +9,36 @@ const findCoachesButton = document.getElementById("find-coaches-button");
 const submitCoachesButton = document.getElementById("submit-coaches-button");
 
 async function getData(event) {
-	const param = playerName.value
+	const playerNameFindParam = playerNameFind.value
 	event.preventDefault();
-	const url = `http://localhost:8080/players?player=${encodeURIComponent(param)}`;
+	const url = `http://localhost:8080/players?player=${encodeURIComponent(playerNameFindParam)}`;
+	
 	try {
 		const response = await fetch(url);
 		if (!response.ok) {
 			throw new Error(`Response status: ${response.status}`);
 		}
-    const result = await response.text();
-    console.log(result);
+		
+    const responseJson = await response.json();
+	
+	const playersDiv = document.getElementById("players");
+	const getDataPlayerNameP = document.createElement("p");
+	const getDataPlayerTeamP = document.createElement("p");
+	const getDataPlayerByteP = document.createElement("p");
+	
+	const getDataPlayerName = responseJson.player
+	const getDataPlayerTeam = responseJson.team
+	const getDataPlayerByte = responseJson.betByte
+	
+	getDataPlayerNameP.textContent = getDataPlayerName
+	getDataPlayerTeamP.textContent = getDataPlayerTeam
+	getDataPlayerByteP.textContent = getDataPlayerByte
+	
+	playersDiv.append(getDataPlayerNameP);
+	playersDiv.append(getDataPlayerTeamP);
+	playersDiv.append(getDataPlayerByteP);
+	
+    console.log(responseJson);
   } catch (error) {
     console.error(error.message);
   }
