3fe8c882911e36b6b4470926cc20e37137f65b9a
IkoBenny <benny.iko.it@gmail.com>
2026-05-08 20:04:05 -0400

    Adds abillity to pull player data from byteBuddyPlayersNba
    On branch master
    Changes to be committed:
    	modified:   actions.js
    	modified:   index.html
    
    You want BACKTICKS for URL:
    
    const url = `http://localhost:8080/players?player=${encodeURIComponent(param)}`;
    
    If button is inside a form:
    
    <form>
       <button id="submit-players-button">
    
    then click triggers form submit/reload.
    
    A fetch may succeed but page reload wipes console immediately.
    
    use below command to prevent default form behavior
    event.preventDefault();

--- actions.js
+++ actions.js
@@ -1,4 +1,5 @@
 const findPlayersButton = document.getElementById("find-players-button");
+const playerName = document.getElementById("player-name-find");
 const submitPlayersButton = document.getElementById("submit-players-button");
 const findTeamsButton = document.getElementById("find-teams-button");
 const submitTeamsButton = document.getElementById("submit-teams-button");
@@ -7,13 +8,27 @@ const submitGamesButton = document.getElementById("submit-games-button");
 const findCoachesButton = document.getElementById("find-coaches-button");
 const submitCoachesButton = document.getElementById("submit-coaches-button");
 
-findPlayersButton.addEventListener("click", (event) => {
-	console.log("findPlayersButton");
-});
+async function getData(event) {
+	const param = playerName.value
+	event.preventDefault();
+	const url = `http://localhost:8080/players?player=${encodeURIComponent(param)}`;
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`);
+		}
+    const result = await response.text();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
 submitPlayersButton.addEventListener("click", (event) => {
 	console.log("submitPlayersButton");
 });
+
+findPlayersButton.addEventListener("click", getData );
 
 findTeamsButton.addEventListener("click", (event) => {
 	console.log("findTeamsButton");
